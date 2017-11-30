f78cf8e75e8e5fabe8000a0664ff874a7e8e5e6b
Nick <Nicholashathaway@gmail.com>
2017-11-29 18:16:17 -0700

    finished step 8

--- app-8/src/App.js
+++ app-8/src/App.js
@@ -1,18 +1,40 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      id: 1,
+      pokemon: [],
+      stats: []
+    }
+  }
+
+  componentDidMount() {
+    axios.get(`https://pokeapi.co/api/v2/pokemon/${this.state.id}`)
+         .then(res => {
+           this.setState({ pokemon: res.data });
+           this.setState({ stats: this.state.pokemon.stats.map(stat => stat.stat.name) });
+        })
+  }
   render() {
+    let poke = this.state.pokemon;
+    let allStats = this.state.stats.map(stat => <p>{ stat }</p>);
+    console.log(this.state.stats);
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        {
+          this.state.pokemon.length === 0 ?
+            <h1>Catching pokemon...</h1> :
+            (
+              <div className='pokeInfo'>
+                <h1>Name: { poke.species.name }</h1>
+                <p>Type: { poke.types[0].type.name }</p>
+                { allStats }
+              </div>
+            )
+        }
       </div>
     );
   }
