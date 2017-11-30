8733fcbc48bcc23a12f241a29c14668a3802cb24
Nick <Nicholashathaway@gmail.com>
2017-11-29 18:27:57 -0700

    finished app-9

---
+++ app-9/src/components/Signup.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function Signup() {
+  return (<div><h1>This is the Signup page</h1></div>);
+}
---
+++ app-9/src/routes.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Switch, Route } from 'react-router';
+
+// components
+import Home from './components/Home';
+import Signup from './components/Signup';
+import Details from './components/Details';
+
+export default (
+  <Switch>
+    <Route exact path='/' component={ Home }/>
+    <Route exact path='/signup' component={ Signup }/>
+    <Route exact path='/details' component={ Details }/>
+  </Switch>
+)
